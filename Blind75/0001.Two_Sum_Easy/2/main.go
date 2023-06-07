12f614af90618426fb509d7166c5b583ac86ecdd
Smita V <81586647+smituvat@users.noreply.github.com>
2023-06-07 13:33:44 +0530

    update

---
+++ Blind75/0001.Two_Sum_Easy/2/main.go
@@ -0,0 +1,32 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+eg:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	fmt.Print(result)
+}
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for idx, num := range nums {
+
+		if val, found := m[target-num]; found {
+			return []int{val, idx}
+		}
+		m[num] = idx
+	}
+	return nil
+}
---
+++ Blind75/0217.Contains_Duplicate_Easy/main.go
@@ -0,0 +1,27 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Eg:
+Input: nums = [1,2,3,1]
+Output: true
+*/
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	isUnique := containsDuplicate(nums)
+	fmt.Print(isUnique)
+
+}
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]int)
+	for k, v := range nums {
+		if _, notunique := m[v]; notunique {
+			return true
+		}
+		m[v] = k
+	}
+	return false
+}
